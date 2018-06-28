230e27d4443b0f10bf8d8ff6a1b82bbaa61e4bbf
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-27 21:13:37 -0700

    Add a -list flag to usernote.

--- usernote/usernote.go
+++ usernote/usernote.go
@@ -8,10 +8,13 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"path/filepath"
+	"sort"
+	"text/tabwriter"
 
 	"bitbucket.org/creachadair/jrpc2"
 	"bitbucket.org/creachadair/jrpc2/channel"
@@ -22,14 +25,14 @@ var (
 	serverAddr   = flag.String("server", os.Getenv("NOTIFIER_ADDR"), "Server address")
 	noteCategory = flag.String("c", "", "Category label (optional)")
 	noteVersion  = flag.String("version", "", "Version to edit")
+	doList       = flag.Bool("list", false, "List matching notes")
 )
 
 func main() {
 	flag.Parse()
-	if flag.NArg() != 1 {
+	if flag.NArg() != 1 && !*doList {
 		log.Fatalf("Usage: %s <tag>", filepath.Base(os.Args[0]))
 	}
-	tag := flag.Arg(0)
 
 	conn, err := net.Dial("tcp", *serverAddr)
 	if err != nil {
@@ -39,11 +42,38 @@ func main() {
 	defer cli.Close()
 	ctx := context.Background()
 
-	if err := cli.Notify(ctx, "Notes.Edit", &notifier.EditNotesRequest{
-		Tag:      tag,
+	if *doList {
+		var rsp []*notifier.Note
+		var tag string
+		if flag.NArg() != 0 {
+			tag = flag.Arg(0)
+		}
+		if err := cli.CallResult(ctx, "Notes.List", &notifier.ListNotesRequest{
+			Tag:      tag,
+			Category: *noteCategory,
+		}, &rsp); err != nil {
+			log.Fatalf("Error listing notes: %v", err)
+		}
+		sort.Slice(rsp, func(i, j int) bool {
+			return noteLess(rsp[i], rsp[j])
+		})
+		tw := tabwriter.NewWriter(os.Stdout, 0, 8, 0, '\t', 0)
+		for _, note := range rsp {
+			fmt.Fprint(tw, note.Tag, "\t", note.Version, "\n")
+		}
+		tw.Flush()
+	} else if err := cli.Notify(ctx, "Notes.Edit", &notifier.EditNotesRequest{
+		Tag:      flag.Arg(0),
 		Category: *noteCategory,
 		Version:  *noteVersion,
 	}); err != nil {
 		log.Fatalf("Error editing note: %v", err)
 	}
+}
+
+func noteLess(a, b *notifier.Note) bool {
+	if a.Tag == b.Tag {
+		return a.Version < b.Version
+	}
+	return a.Tag < b.Tag
 }
