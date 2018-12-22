c80c24965ea4edcb58374a48eb7538be33e42d63
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-12-22 14:36:50 -0800

    config: Add hooks for auth token settings.

--- config.go
+++ config.go
@@ -3,10 +3,14 @@ package notifier
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 
+	"bitbucket.org/creachadair/jrpc2/jauth"
+	"bitbucket.org/creachadair/jrpc2/jctx"
 	"bitbucket.org/creachadair/shell"
 )
 
@@ -14,6 +18,8 @@ import (
 type Config struct {
 	Address  string `json:"address"`
 	DebugLog bool   `json:"debugLog"`
+
+	Auth AuthConfig `json:"auth,omitempty"`
 
 	// Settings for the clipboard service.
 	Clip struct {
@@ -67,4 +73,50 @@ func (c *Config) EditFile(ctx context.Context, path string) error {
 	args, _ := shell.Split(c.Edit.Command)
 	bin, rest := args[0], args[1:]
 	return exec.CommandContext(ctx, bin, append(rest, path)...).Run()
+}
+
+// AuthConfig specifies a mapping of usernames to authorization rules.
+type AuthConfig map[string]ACL
+
+// An ACL represents a set of access rights protected by a key.  If the list of
+// rules is empty, all methods are accessible.
+type ACL struct {
+	Key   string   `json:"key"`
+	Rules []string `json:"acl,omitempty"`
+}
+
+func (a ACL) checkMethod(method string) bool {
+	for _, rule := range a.Rules {
+		ok, err := filepath.Match(rule, method)
+		if err == nil && ok {
+			return true
+		}
+	}
+	return len(a.Rules) == 0
+}
+
+// CheckAuth checks whether the specified request is authorized.
+func (a AuthConfig) CheckAuth(ctx context.Context, method string, params []byte) error {
+	if a == nil {
+		return nil
+	}
+	raw, ok := jctx.AuthToken(ctx)
+	if !ok {
+		return errors.New("no authorization token")
+	}
+	tok, err := jauth.ParseToken(raw)
+	if err != nil {
+		return jauth.ErrInvalidToken
+	}
+	acl, ok := a[tok.User]
+	if !ok {
+		return errors.New("unknown user")
+	} else if !acl.checkMethod(method) {
+		return errors.New("method not allowed")
+	}
+
+	return jauth.User{
+		Name: tok.User,
+		Key:  []byte(acl.Key),
+	}.VerifyParsed(tok, method, params)
 }
