05e4b4a04493d3c46908af2c5a1e7be92da12338
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-12-26 08:38:32 -0800

    noteserver: Clean up handling of config.
    Remove the detailed configuration flags, leaving only -address and -debuglog.
    These now override the configuration file; everything else must be saved in the
    file itself.

--- noteserver/noteserver.go
+++ noteserver/noteserver.go
@@ -30,33 +30,23 @@ var (
 	cfg notifier.Config
 	lw  *log.Logger
 
-	configPath = flag.String("config", "", "Configuration file path (overrides other flags)")
+	configPath = flag.String("config", "", "Configuration file path")
+	serverAddr = flag.String("address", "", "Server address (overrides config)")
+	debugLog   = flag.Bool("debuglog", false, "Enable debug logging (overrides config)")
 )
-
-func init() {
-	flag.StringVar(&cfg.Address, "address", os.Getenv("NOTIFIER_ADDR"), "Server address")
-	flag.StringVar(&cfg.Edit.Command, "editor", os.Getenv("EDITOR"), "Editor command line")
-	flag.StringVar(&cfg.Note.Sound, "sound", "Glass", "Sound name to use for audible notifications")
-	flag.StringVar(&cfg.Note.Voice, "voice", "Moira", "Voice name to use for voice notifications")
-	flag.BoolVar(&cfg.DebugLog, "log", false, "Enable debug logging")
-}
 
 func main() {
 	flag.Parse()
-	if err := notifier.LoadConfig(*configPath, &cfg); err != nil {
+	if *configPath == "" {
+		log.Fatal("You must provide a non-empty -config file path")
+	} else if err := notifier.LoadConfig(*configPath, &cfg); err != nil {
 		log.Fatalf("Loading configuration: %v", err)
 	}
-	if cfg.Address == "" {
-		log.Fatal("A non-empty --address is required")
-	} else if cfg.DebugLog {
-		lw = log.New(os.Stderr, "[noteserver] ", log.LstdFlags)
+	if *serverAddr != "" {
+		cfg.Address = *serverAddr
 	}
-	opts := &jrpc2.ServerOptions{
-		Logger:        lw,
-		Metrics:       metrics.New(),
-		StartTime:     time.Now().In(time.UTC),
-		DecodeContext: jctx.Decode,
-		CheckAuth:     cfg.Auth.CheckAuth,
+	if cfg.DebugLog || *debugLog {
+		lw = log.New(os.Stderr, "[noteserver] ", log.LstdFlags)
 	}
 
 	lst, err := net.Listen("tcp", cfg.Address)
@@ -64,7 +54,13 @@ func main() {
 		log.Fatalf("Listen: %v", err)
 	}
 	if err := server.Loop(lst, notifier.PluginAssigner(&cfg), &server.LoopOptions{
-		ServerOptions: opts,
+		ServerOptions: &jrpc2.ServerOptions{
+			Logger:        lw,
+			Metrics:       metrics.New(),
+			StartTime:     time.Now().In(time.UTC),
+			DecodeContext: jctx.Decode,
+			CheckAuth:     cfg.Auth.CheckAuth,
+		},
 	}); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
