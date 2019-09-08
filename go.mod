3da7f265ccd8b03cf2afcaccd44cd70f67c1ca25
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-09-08 11:09:47 -0700

    postnote: Add an -exec flag to run and time a command.

--- go.mod
+++ go.mod
@@ -11,9 +11,9 @@ require (
 	github.com/creachadair/keyfish v0.0.29
 	github.com/google/go-cmp v0.3.1 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586
-	golang.org/x/sys v0.0.0-20190826163724-acd9dae8e8cc // indirect
+	golang.org/x/crypto v0.0.0-20190907121410-71b5226ff739
+	golang.org/x/sys v0.0.0-20190907184412-d223b2b6db03 // indirect
 	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20190709130402-674ba3eaed22
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20190905181640-827449938966
 )
--- postnote/postnote.go
+++ postnote/postnote.go
@@ -8,13 +8,19 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"os/exec"
 	"strings"
+	"time"
 
+	"bitbucket.org/creachadair/shell"
 	"github.com/creachadair/notifier"
 )
 
 var (
+	doExec       = flag.Bool("exec", false, "Execute a command and post a notice when it completes")
 	noteTitle    = flag.String("title", "", "Notification title")
 	noteSubtitle = flag.String("subtitle", "", "Notification subtitle")
 	noteAudible  = flag.Bool("audible", false, "Whether notification should be audible")
@@ -26,7 +32,22 @@ func init() { notifier.RegisterFlags() }
 func main() {
 	flag.Parse()
 	var title, body string
-	if *noteTitle != "" {
+	if *doExec {
+		if flag.NArg() == 0 {
+			log.Fatal("You must provide a command to execute with -exec")
+		}
+		start := time.Now()
+		cmd := exec.Command(flag.Arg(0), flag.Args()[1:]...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		title = "Command complete"
+		if err := cmd.Run(); err != nil {
+			title = fmt.Sprintf("Command failed: %v", err)
+		}
+		body = shell.Join(flag.Args())
+		body += fmt.Sprintf("\n%v elapsed", time.Since(start).Truncate(1*time.Millisecond))
+	} else if *noteTitle != "" {
 		title = *noteTitle
 		body = strings.Join(flag.Args(), " ")
 	} else if flag.NArg() == 0 {
